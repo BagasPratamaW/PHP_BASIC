58242a8adceaebd3d3e91c5adceb3e74ce77705c
Bagas P Wibisono <53308520+BagasPratamaW@users.noreply.github.com>
2019-10-09 09:55:44 +0800

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,57 @@
+<?php 
+    // echo "Hello World !";
+    // echo "<h1>Hello World !</h1>";
+
+    // $nama = "Bagas Pratama<br>";
+    // $pesan = "Tetaplah <strong>Bernafas</strong>";
+
+    // echo $nama;
+    // echo $pesan;
+
+    //~~~~~~~~~~~~~~~~//
+    // MATEMATIKA PHP//
+    //~~~~~~~~~~~~~~//
+    // $a = 17;
+    // $b = 23;
+
+    // echo $a + $b;
+    // echo "<br>";
+    // echo $a - $b;
+    // echo "<br>";
+    // echo $a * $b;
+    // echo "<br>";
+    // echo $a % $b;
+    // echo "<br>";
+    // echo $a / $b;
+
+      //~~~~~~~~~~~~~~//
+     // 2 Variabel   //
+    //~~~~~~~~~~~~~~//
+    // $nama_depan = "Bagas Pratama";
+    // $nama_belakang = " Wibisono";
+    // echo $nama_depan . " " . $nama_belakang;
+
+     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    // Assigntment 2 /  lebih var //
+   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+/*
+    $x = "Bagas";
+    $x .= " ";
+    $x .= "Pratama";
+    echo $x;
+   
+    echo "<br>"
+
+    $angka = 10;
+    $angka += 5;
+    $angka += 3;
+    echo $angka;
+    */
+  
+    // PERBANDINGAN / COMPARATION //
+    // ==, <=, >=, <, >, !=  //
+
+    //iNDENTITAS /STRICT // 
+    // ===, !===
+
+?>
